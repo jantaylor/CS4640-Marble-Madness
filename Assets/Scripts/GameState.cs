e68ec0ca0cb5553743cf401d885b75cae0e04703
Jan Taylor <janttaylor@gmail.com>
2018-04-01 18:32:58 -0600

    GameState ready

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour {
+
+    /// <summary>
+    /// Create an instance of the game state object in memory
+    /// </summary>
+    public static GameState Instance;
+
+    /// <summary>
+    /// Scene name
+    /// </summary>
+    [SerializeField]
+    private int activeScene;
+
+    /// <summary>
+    /// List of Scene Names hard coded
+    /// </summary>
+    [SerializeField]
+    private string[] scenes = new string[] {
+        "Menu",
+        "Level1"
+    };
+
+    /// <summary>
+    /// Set and get active scene
+    /// </summary>
+    public int ActiveScene {
+        get { return activeScene; }
+        set { activeScene = value; }
+    }
+
+    /// <summary>
+    /// On starting the game, singleton state created
+    /// </summary>
+    void Awake() {
+        if (Instance == null) {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        activeScene = 0; // 0 is menu, 1 is level 1
+    }
+
+    /// <summary>
+    /// Load previous Scene
+    /// </summary>
+    public void LoadPreviousScene() {
+        // Load the Scene
+        if (ActiveScene != 0) {
+            SceneManager.LoadScene(scenes[ActiveScene -= 1]);
+        } else {
+            LoadMenu();
+        }
+    }
+
+    /// <summary>
+    /// Load next Scene
+    /// </summary>
+    public void LoadNextScene() {
+        // Load the Scene based on last scene
+        if (ActiveScene != scenes.Length - 1) {
+            SceneManager.LoadScene(scenes[ActiveScene += 1]);
+        } else {
+            LoadMenu();
+        }
+    }
+
+    /// <summary>
+    /// Load the menu
+    /// </summary>
+    public void LoadMenu() {
+        // Load the menu
+        ActiveScene = 0; // menu scene
+        SceneManager.LoadScene(ActiveScene);
+    }
+}
