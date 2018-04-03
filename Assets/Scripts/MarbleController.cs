372cc4ed42fc0761ceba59c9e389f80486887197
Jan Taylor <janttaylor@gmail.com>
2018-04-03 09:50:23 -0600

    Made horizontal movement faster

--- Assets/Scripts/MarbleController.cs
+++ Assets/Scripts/MarbleController.cs
@@ -36,7 +36,7 @@ public class MarbleController : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal") * 1.2f;
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
