3d1bcc43d57c676171932a9b422bc070cd2fdc67
Andrewid <AndrewMerrell1@mail.weber.edu>
2018-04-06 01:12:00 -0600

    Level1b
    Railings look better and make a sound when collided
    
    The camera currently plays the level music

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour {
 	
+    public int numPlayersWon = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +14,30 @@ public class Goal : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// When a player enters the goal,
+    /// they are awarded first or second place
+    /// their movement stops
+    /// their score is updated based on timer remaining
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerExit(Collider other)
+    {
+        // do we assume that a player triggered?
+        // sure why not
+        numPlayersWon++;
+
+        if (other.CompareTag("Player")) {
+            // check timeleft
+            // update score based on their tag
+            // cease other.movement
+            // translate to "1st place" location
+            // or 2ndplace
+            // or nth place? how may will we have?
+
+
+        }
+        //do same 
+    }
 }
---
+++ Assets/SoundOnCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnCollide : MonoBehaviour {
+	
+    /// <summary>
+    /// The audio source.
+    /// </summary>
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other) {
+        audioSource.Play();
+    }
+}
