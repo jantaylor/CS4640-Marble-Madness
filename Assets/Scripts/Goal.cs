174c79d5db8759b70b822f1a638c9c697cae85c9
Andrewid <AndrewMerrell1@mail.weber.edu>
2018-04-11 20:49:54 -0600

    added some logic to Goal
    rounded the collider's ints
    retextured the hammer
    I plan to "rig" the Marble Muncher
    
    Goal script is Untested

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,7 +7,8 @@ public class Goal : MonoBehaviour {
     /// <summary>
     /// The audio source.
     /// </summary>
-    public AudioSource[] audioSources;
+    //public AudioSource[] audioSources; 
+    //players get their own goal sound attached to them instead
 	
     public int numPlayersWon = 0;
 	// Use this for initialization
@@ -28,26 +29,43 @@ public class Goal : MonoBehaviour {
     /// their score is updated based on timer remaining
     /// </summary>
     /// <param name="other"></param>
-    void OnTriggerEnter(Collider other)
-    {
+    void OnTriggerEnter(Collider other) {
         // do we assume that a player triggered?
         // sure why not
         numPlayersWon++;
+        AudioSource GoalSound = other.GetComponent(AudioSource);
+        GoalSound.Play();
 
         if (other.CompareTag("Player")) {
+            
             // check timeleft
             // update score based on their tag
             // cease other.movement
             // translate to "1st place" location
             // or 2ndplace
-            // or nth place? how may will we have?
+            // or nth place? how may will we have?  Ok, just 2 for now
             // Play the audiosource attached to whatever player entered
             // or 0 = player 1
-
-            audioSources[0].Play();//Which player?
-
+            if (other.GetComponent(name).Equals("Player1")) 
+                // Not sure if that works
+                GameManager.playerOneFinished = true;
+            
+            if (other.GetComponent(name).Equals("Player2")) 
+                // Not sure if that works
+                GameManager.playerTwoFinished = true;
+            
         }
         // when maxplayers and numplayers are the same, queue next level
         // Need to set instance gameManager.levelComplete = true;
+        if (!GoalSound.isPlaying) {
+            if (GameState.Instance.TwoPlayers) {
+                if (numPlayersWon == 2)
+                    GameState.Instance.LoadNextScene();
+                //is this even necessary? I mean the else part
+            } else {
+                if (numPlayersWon == 1)
+                    GameState.Instance.LoadNextScene();
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -3,14 +3,20 @@ A near remake of the classic game
 
 Player 1's controls are up-down-left-right
 
+
 We had some issues with broken pre-fabs while learning to merge
+
 
 had to re-design level 1 twice
 
+
 The bars make a sound when bumped into
+
 
 Goal makes a sound, but you can't hear it because it switches to the menu to quick
 
+
 we'll fix that
+
 
 as well as show you the score and transition you to the next level
