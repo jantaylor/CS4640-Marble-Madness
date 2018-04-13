d9b702e456c7a40499e131eaaede90fa67d45b23
Andrewid <AndrewMerrell1@mail.weber.edu>
2018-04-13 14:58:16 -0600

    More work, sorry no animation quite yet

--- Assets/Scripts/PlayerDeathOnCollision.cs
+++ Assets/Scripts/PlayerDeathOnCollision.cs
@@ -6,7 +6,10 @@ public class PlayerDeathOnCollision : MonoBehaviour {
 	
     void OnTriggerEnter(Collider other) {
         string sWhoDied = other.GetComponent(name);
-        string sHowTheyDied = gameObject.name;
-
+        string sHowTheyDied = gameObject.GetComponent(name);
+        // return to closest respawn area 
+        // Maybe have a respawnarea give a player a vec3 location where 
+        // it can respawn on trigger enter (or exit)
+        // have the starting locations be respawnAreas
     }
 }
