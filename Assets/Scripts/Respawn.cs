201a2e4647e4653f59fa1f340044a516ab9a631a
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-04-14 15:06:32 -0600

    Respawn working
    Respawn working for Player1 and Player2

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Respawn : MonoBehaviour
+{
 
-    public Transform respawnPoint = null;
-    public GameObject player1;
-    
+    public GameObject respawnPoint; //respawn point for player 1
+    public GameObject player2RespawnPoint; //respawn point for player 2
+    public GameObject player1; //game object for player one
+    public GameObject player2; //game object for player 2
 
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        player1.transform.position = respawnPoint.position;
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+    /// <summary>
+    /// If the collider is triggered it will scan for the tags and if the tag is 
+    /// player 1 or player 2 it will take the player back to the appropriate respawn point.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player2"))
+        {
+            player2.transform.position = respawnPoint.transform.position;
+        }
+        else
+        {
+            player1.transform.position = player2RespawnPoint.transform.position;
+        }
     }
 }
