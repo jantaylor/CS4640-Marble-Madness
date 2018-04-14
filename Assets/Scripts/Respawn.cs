f7c6f33542e4aeb0a8731617a97f0128a276c90c
Elizabeth Ruttenbur <elizabethruttenbur@gmail.com>
2018-04-14 14:14:45 -0600

    Respawn
    Respawn.cs

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Transform respawnPoint = null;
+    public GameObject player1;
+    
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player1.transform.position = respawnPoint.position;
+    }
+}
