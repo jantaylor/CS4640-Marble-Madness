c036d4f7b12ebd846ccbf5d62922f95e762f8163
Jan Taylor <janttaylor@gmail.com>
2018-04-22 10:51:01 -0600

    Fixes to levels and starting hide script for top platform

---
+++ Assets/Scripts/HideLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideLevel : MonoBehaviour {
+
+    public GameObject platform;
+
+    //void OnTriggerEnter(Collider other) {
+        // Hide/disable the parent object when the player hits this.
+        // Of course this won't work in 2 player mode due to one player may remain on the platform (screen cheaters)
+        //platform.gameObject.setEnabled(false);
+    //}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
